ce0d9e7533b589325879dd3ed58e0457c536e6ae
adikchanski <adik.sabirov.zso@gmail.com>
2026-03-11 00:07:26 +0100

    first change

--- apbd-2026-eng-13c/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace apbd_2026_eng_13c;
-
-public class Class1
-{
-}
---
+++ apbd-2026-eng-13c/Program.cs
@@ -0,0 +1,10 @@
+namespace apbd_2026_eng_13c;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("First change");
+    }
+}
